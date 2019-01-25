b68ea56dbe38e25e23a8d90905c7af3f254461d5
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-01-25 20:06:43 +0100

    Reviewing previous chapters

---
+++ Chapter Review.R
@@ -0,0 +1,14 @@
+#Previous knowledge testing
+infoA <- c(7.5, 3.8, 9.6)
+infoA
+
+#Vector with names
+nameVector <- c("Paradiso", "Dracula", "Shawshank")
+
+#Add names to infoA
+names(infoA) <- nameVector
+infoA
+
+#Creating a matrix
+spremenljivka <- matrix(1:54, byrow = TRUE, ncol = 6)
+spremenljivka
