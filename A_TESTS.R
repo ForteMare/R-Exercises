3e6805a31f8491250b9e8820ea2c436fa31af3f6
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-01-25 21:58:57 +0100

    Update A_TESTS.R

--- A_TESTS.R
+++ A_TESTS.R
@@ -6,8 +6,31 @@ names(poker_vector) <- days_vector
 names(roulette_vector) <- days_vector
 
 # Which days did you make money on poker?
-selection_vector <- poker_vector < 0
+selection_vector <- poker_vector > 0
 
 # Select from poker_vector these days
 poker_winning_days <- poker_vector[selection_vector]
 poker_winning_days
+
+# Star Wars Exercise
+
+# Construct star_wars_matrix
+box_office <- c(460.998, 314.4, 290.475, 247.900, 309.306, 165.8)
+star_wars_matrix <- matrix(box_office, nrow = 3, byrow = TRUE,
+                           dimnames = list(c("A New Hope", "The Empire Strikes Back", "Return of the Jedi"), 
+                                           c("US", "non-US")))
+
+# The worldwide box office figures
+worldwide_vector <- rowSums(star_wars_matrix)
+
+# Bind the new variable worldwide_vector as a column to star_wars_matrix
+all_wars_matrix <- cbind(star_wars_matrix, worldwide_vector)
+
+# Working with factors 
+factorsGroup <- c(1, 5, 9, 2, 2, 2, 1, 5)
+factoring <- factor(factorsGroup)
+
+
+factoring
+
+
