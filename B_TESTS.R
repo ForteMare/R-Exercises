9b7d0595ac8f62be7521f7d470e166f8193b43cd
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-01-26 20:15:18 +0100

    Added final to tester

---
+++ B_TESTS.R
@@ -0,0 +1,20 @@
+#Call data frame 
+mtcars
+
+# A Vector 
+vecA <- 1:4
+names(vecA) <- c("A", "B", "C", "D")
+vecA
+
+# B Vector 
+vecB <- c("Timmy", "Jordie", "Sam", "Cherry")
+names(vecB) <- c("First", "Second", "Third", "Fourth")
+vecB
+
+# Add to dataframe
+firstDataFarme <-data.frame(vecB, vecA)
+
+firstDataFarme
+
+
+
