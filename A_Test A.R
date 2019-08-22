b98f00806b77ee206fb0ec21e31739c31b042ee8
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-08-22 18:07:36 +0200

    Playing with vectors

--- A_Test A.R
+++ A_Test A.R
@@ -10,3 +10,14 @@ x
 k <- c(5, 7, 8, 13)
 z <-k
 z
+
+# Vectors of equal size test
+eq1 <- c(5, 12, 13)
+eq2 <- c(14, 7, 10)
+eq1 + eq2
+
+# Vectors of unequal size test
+ueq1 <- c(4, 7, 9)
+ueq2 <- c(5, 10)
+ueq1 + ueq2 #Displays a warning message and continues adding by repeating the vector
+
