2a61cf597710532e76629e45e2d569a5c2458822
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-08-23 12:15:49 +0200

    Changed so that naming method example uses data from above

--- A_CheatSheet A.R
+++ A_CheatSheet A.R
@@ -1,7 +1,7 @@
 # Setting up some data for examples below
 even <- c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
 odd <- c(1, 3, 5, 7, 9, 11, 13, 15, 17, 19)
-week <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+week <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 weektemp <- c(22, 27, 31, 25, 33, 34, 35)
 
 # Arithmetic result 
@@ -28,10 +28,8 @@ ueq2 <- c(5, 10)
 ueq1 + ueq2 #Displays a warning message and continues adding by repeating the vector
 
 # Naming objects in vectors
-days <- c("Monday", "Tuesday", "Wednesday")
-degrees <- c(15, 17, 16)
-names(degrees) <- days
-degrees
+names(weektemp) <- week
+weektemp
 
 # Sum calculates all elements in a vector
 sum(degrees)
@@ -39,3 +37,8 @@ sum(degrees)
 # Selecting elements in a vector. Numbers go from 1 to vector size
 degrees[2]
 
+# Selecting some elements in a vector
+wkdays <- week[c(1, 2, 3, 4, 5)]
+wkdays
+wkends <- week[c(6, 7)]
+wkends
