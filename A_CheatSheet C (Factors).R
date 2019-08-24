849fb4848e71794912da602a0a98bf027a5cad57
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-08-24 23:29:10 +0200

    More info on factors

---
+++ A_CheatSheet C (Factors).R
@@ -0,0 +1,17 @@
+# Factors are used for categorical variables, like gender
+gender_vector <- c("Male", "Female", "Female", "Male", "Male")
+
+# Use a factor() function to make a factor
+factor_gender_vector <- factor(gender_vector)
+factor_gender_vector #Displays what factors exist within the vector
+
+# **NOTICE:**
+# Nominal categorical variable is without an implied order, like gender
+# Ordinal categorical variable is ordered, such as temperature (low, med., high)
+
+# Ordering variables
+temperature_vector <- c("LOW", "MEDIUM", "HIGH")
+factor_temperature_vector <- factor(temperature_vector, ordered = TRUE, levels = temperature_vector)
+# levels = temperature vector is short-hand for c(...)
+factor_temperature_vector
+
