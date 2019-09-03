5f0b3e07ede0ed42eb7a97da0d4ebe3c4939300f
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-09-03 18:00:27 +0200

    Completed 1a

--- FIS Statistics Seminars/S2 35160133.R
+++ FIS Statistics Seminars/S2 35160133.R
@@ -3,10 +3,21 @@
 # ----------------- 1. ARITHMETIC MEAN -----------------
 
 # 1A - Import data into R 
-imported_raw_data <- read.csv(file = "C:\\Users\\Sapun\\Desktop\\S\\APPDATA.csv", header = TRUE, sep = ',')
-View(imported_raw_data)
+imported_raw_data <- read.csv(file = "C:\\Users\\Sapun\\Desktop\\S\\APPDATA.csv", 
+                              header = TRUE, sep = ',')
+#View(imported_raw_data)
 
+# Renaming variables 
 assigned_vars <- c("price", "cont_rating", "prime_genre", "lang.num")
 renamed_vars <- c("price", "PG", "genre", "languages") # PG is short for Parental Guidance
+
+my_data <- imported_raw_data[assigned_vars]
+my_data
+
+names(my_data) <- renamed_vars
+View(my_data)
+
+# 1B - Arithmetic mean for numeric variables
+
 # ------------------------------------------------------
 
