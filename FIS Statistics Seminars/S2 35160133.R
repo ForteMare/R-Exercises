64e389c216dcb9be2c85e4b280064378f965a97c
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-09-03 17:52:55 +0200

    Question 1, importing data

--- FIS Statistics Seminars/S2 35160133.R
+++ FIS Statistics Seminars/S2 35160133.R
@@ -2,4 +2,11 @@
 
 # ----------------- 1. ARITHMETIC MEAN -----------------
 
+# 1A - Import data into R 
+imported_raw_data <- read.csv(file = "C:\\Users\\Sapun\\Desktop\\S\\APPDATA.csv", header = TRUE, sep = ',')
+View(imported_raw_data)
+
+assigned_vars <- c("price", "cont_rating", "prime_genre", "lang.num")
+renamed_vars <- c("price", "PG", "genre", "languages") # PG is short for Parental Guidance
 # ------------------------------------------------------
+
